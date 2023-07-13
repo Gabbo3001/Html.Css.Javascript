ac5b7837b066690a74f7909649cf0ebe800c3e5d
JGabriel Bustinza <114235606+Gabbo3001@users.noreply.github.com>
2023-07-13 13:12:19 -0500

    update

---
+++ TO DO LIST/login.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form");
+  const usuario = document.getElementById("usuario");
+  const contraseña = document.getElementById("contraseña");
+  const parrafo = document.getElementById("warnings");
+
+  // Agregar los datos de usuario y contraseña al almacenamiento local
+  localStorage.setItem("usuario", "Gabriel");
+  localStorage.setItem("contraseña", "3001");
+
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const storedUsuario = localStorage.getItem("usuario");
+    const storedContraseña = localStorage.getItem("contraseña");
+
+    if (usuario.value !== storedUsuario || contraseña.value !== storedContraseña) {
+      parrafo.innerHTML = "Usuario o contraseña incorrectos";
+    } else {
+      parrafo.innerHTML = ""; // Vaciar el contenido del párrafo si los datos son correctos
+      window.location.href = "index.html"; // Redirigir a otra-pagina.html
+    }
+  });
+});
