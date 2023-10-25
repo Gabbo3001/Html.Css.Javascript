378aa6794083b35848eb0103196d554328f2f057
JGabriel Bustinza <114235606+Gabbo3001@users.noreply.github.com>
2023-10-24 21:07:18 -0500

    Prueba de Codeable

---
+++ Prueba1/main.js
@@ -0,0 +1,24 @@
+
+const lista = document.getElementById("listaDeNotas");
+const input = document.getElementById("ingresarNota");
+const CrearNotaBoton = document.getElementById("crearNota");
+
+CrearNotaBoton.addEventListener("click", function() {
+
+    const textoNota = input.value.trim();
+
+    if (textoNota !== "") {
+        const nota = document.createElement("li");
+        const botonBorrar = document.createElement("button");
+        nota.textContent = textoNota;
+        botonBorrar.textContent = "Borrar";
+        botonBorrar.id = "borrar";
+        botonBorrar.addEventListener("click", function() {
+            nota.remove();
+        });
+        nota.appendChild(botonBorrar);
+        lista.appendChild(nota);
+
+        input.value = ""; //limpiar
+    }
+});
